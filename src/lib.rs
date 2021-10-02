cbd0125865d25b1a8f750200e94181565a6bc44c
York Xiang <bombless@126.com>
2021-10-02 11:34:54 +0800

    Finish `feedback` implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,78 @@
 pub struct GameState;
 
 #[derive(PartialEq, Eq)]
-pub struct Location(u8);
+
+#[derive(Clone, Copy)]
+pub struct Location(i8);
 
 pub fn to_location(s: &str) -> Option<Location> {
-    Some(Location(4 * (s.bytes().nth(0).unwrap() - b'A') + s.bytes().nth(1).unwrap() - b'1'))
+    Some(Location((4 * (s.bytes().nth(0).unwrap() - b'A') + s.bytes().nth(1).unwrap() - b'1') as i8))
 }
 
 pub fn from_location(Location(loc): Location) -> String {
-    format!("{}{}", (loc / 4 + b'A') as char, (loc % 4 + b'1') as char)
+    format!("{}{}", (loc as u8 / 4 + b'A') as char, (loc as u8 % 4 + b'1') as char)
+}
+
+pub fn feedback(target: [Location; 3], guess: [Location; 3]) -> (i32, i32, i32) {
+    let mut ret = (0, 0, 0);
+    for guess in guess {
+        if feedback_exact(target, guess) > 0 {
+            ret.0 += 1;
+            continue;
+        };
+        let onespace = feedback_onespace(target, guess);
+        if onespace > 0 {
+            ret.1 += onespace;
+            continue;
+        }
+        let twospace = feedback_twospace(target, guess);
+        if twospace > 0 {
+            ret.2 += twospace;
+            continue;
+        }
+    }
+    return ret;
+}
+
+fn feedback_exact(target: [Location; 3], guess: Location) -> i32 { // one or zero
+    for i in 0 .. 3 {
+        if target[i] == guess {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+fn feedback_onespace(target: [Location; 3], Location(guess): Location) -> i32 { // one or zero
+    let onespace = [
+        guess - 4 - 1, guess - 4, guess - 4 + 1,
+        guess - 1, guess + 1,
+        guess + 4 - 1, guess + 4, guess + 4 + 1,
+    ];
+    for i in 0 .. 3 {
+        if onespace.contains(&target[i].0) {
+            println!("guess {} matching onespace from {}",
+                from_location(Location(guess)), from_location(target[i]));
+            return 1;
+        }
+    }
+    return 0;
+}
+
+fn feedback_twospace(target: [Location; 3], Location(guess): Location) -> i32 { // one or zero
+    let twospace = [
+        guess - 8 - 2, guess - 8 - 1, guess - 8, guess - 8 + 1, guess - 8 + 2,
+        guess - 4 - 2, guess - 4 + 2,
+        guess - 2, guess + 2,
+        guess + 4 - 2, guess + 4 + 2,
+        guess + 8 - 2, guess + 8 - 1, guess + 8, guess + 8 + 1, guess + 8 + 2,
+    ];
+    for i in 0 .. 3 {
+        if twospace.contains(&target[i].0) {
+            return 1;
+        }
+    }
+    return 0;
 }
 
 #[cfg(test)]
--- src/tests.rs
+++ src/tests.rs
@@ -26,3 +26,33 @@ fn test_from_location() {
     assert_eq!("A1", from_location(a1));
     assert_eq!("B1", from_location(b1));
 }
+
+#[test]
+fn test_feedbacks() {
+    test(("H1", "B2", "D3"), ("B3", "C3", "H3"), (0, 2, 1));
+    test(("H1", "B2", "D3"), ("B1", "A2", "H3"), (0, 2, 1));
+    test(("H1", "B2", "D3"), ("B2", "H2", "H1"), (2, 1, 0));
+    test(("A1", "D2", "B3"), ("A3", "D2", "H1"), (1, 1, 0));
+    test(("A1", "D2", "B3"), ("H4", "G3", "H2"), (0, 0, 0));
+    test(("A1", "D2", "B3"), ("D2", "B3", "A1"), (3, 0, 0));
+}
+
+
+fn test(locations: (&str, &str, &str), guess: (&str, &str, &str), feedback: (i32, i32, i32)) {
+    use crate::to_location;
+    let locations_array = [
+        to_location(locations.0).unwrap(),
+        to_location(locations.1).unwrap(),
+        to_location(locations.2).unwrap(),
+    ];
+
+    let guess_array = [
+        to_location(guess.0).unwrap(),
+        to_location(guess.1).unwrap(),
+        to_location(guess.2).unwrap(),
+    ];
+
+    let value = crate::feedback(locations_array, guess_array);
+    assert_eq!(value, feedback,
+        "{:?} {:?} expect {:?} got {:?}", locations, guess, feedback, value);
+}
